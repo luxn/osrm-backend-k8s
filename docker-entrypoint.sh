4e5e9df8ccac84138480708e7ed6d380a14f7951
luxn <maximilian.herbers@outlook.com>
2020-01-26 20:42:54 +0100

    modified k8s deployment files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,8 @@ fi
 OSRM_DATA_PATH=${OSRM_DATA_PATH:="/osrm-data"}
 OSRM_DATA_LABEL=${OSRM_DATA_LABEL:="data"}
 OSRM_GRAPH_PROFILE=${OSRM_GRAPH_PROFILE:="car"}
-OSRM_PBF_URL=${OSRM_PBF_URL:="http://download.geofabrik.de/asia/maldives-latest.osm.pbf"}
+OSRM_GRAPH_PROFILE_URL=${OSRM_GRAPH_PROFILE_URL:=""}
+OSRM_PBF_URL=${OSRM_PBF_URL:="https://download.geofabrik.de/europe/germany/hamburg-latest.osm.pbf"}
 
 # AWS variables
 OSRM_AWS_ACCESS_KEY_ID=${OSRM_AWS_ACCESS_KEY_ID:=""}
@@ -28,12 +29,24 @@ _sig() {
 trap _sig SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
 
+# Set the graph profile path
+OSRM_GRAPH_PROFILE_PATH="/osrm-profiles/$OSRM_GRAPH_PROFILE.lua"
+
+# If the URL to a custom profile is provided override the default profile
+if [ ! -z "$OSRM_GRAPH_PROFILE_URL" ]; then
+    # Set the custom graph profile path
+    OSRM_GRAPH_PROFILE_PATH="/osrm-profiles/custom-profile.lua"
+    # Retrieve the custom graph profile
+    curl -L $OSRM_GRAPH_PROFILE_URL --create-dirs -o $OSRM_GRAPH_PROFILE_PATH
+fi
+
+
 if [ "$OSRM_MODE" == "CREATE" ]; then    
     # Retrieve the PBF file
     curl -L $OSRM_PBF_URL --create-dirs -o $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf
     
     # Build the graph
-    osrm-extract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf -p /osrm-profiles/$OSRM_GRAPH_PROFILE.lua
+    osrm-extract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osm.pbf -p $OSRM_GRAPH_PROFILE_PATH
     osrm-contract $OSRM_DATA_PATH/$OSRM_DATA_LABEL.osrm
 
     if [ ! -z "$OSRM_AWS_ACCESS_KEY_ID" ] && [ ! -z "$OSRM_AWS_SECRET_ACCESS_KEY" ] && [ ! -z "$OSRM_AWS_S3_BUCKET" ] && [ ! -z "$OSRM_AWS_S3_BUCKET" ]; then    
